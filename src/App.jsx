455210fed4a3ed222e5ff5e0f9b067fe7e280afb
Waqas03ahmad <vickytrader03@gmail.com>
2024-04-18 20:00:03 -0700

    secon2d

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <>Waqas ahmad</>;
+  return (
+    <>
+      <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8" />
+    </>
+  );
 }
 
 export default App;
