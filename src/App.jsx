719cdb3b9f2745c1c2e104f9271705fd674a1c93
Waqas03ahmad <vickytrader03@gmail.com>
2024-04-19 04:00:06 -0700

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./Index";
+
 function App() {
   return (
     <>
-      <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8" />
+      <Header />
     </>
   );
 }
